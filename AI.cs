24a143317635560c312e005d1ad5030e66721752
Balinto <balintm2006@gmail.com>
2024-03-30 14:57:49 +0100

    Ai class megcsinalva

---
+++ AI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineStep
+{
+    public class AI
+    {
+        public string[,] Display { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public AI(string[,] display, int x, int y)
+        {
+            Display = display;
+            X = x;
+            Y = y;
+        }
+
+        private string curTile()
+        {
+            return Display[X, Y];
+        }
+
+        public async Task Move()
+        {
+            if (curTile() == "0")
+            {
+                X++;
+            }
+            else
+            {
+                // Ide kell az AI logika
+            }
+        }
+
+    }
+}
