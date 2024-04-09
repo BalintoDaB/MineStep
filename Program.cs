c2c7a63a50bc6d1bcf9b878df28d8c1ba077bf8e
Lorino <nemeth.lorin.dorina@students.jedlik.eu>
2024-04-09 17:32:14 +0200

    Menu(meg nem kesz)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,39 @@
-﻿using MineStep;
+﻿using Kocsiszin;
+using MineStep;
 
-Player player = new Player(0, 0, "P", new int[] { 20, 10 });
-Map map = new Map(20, 10, 10, player);
-await map.Run();
+
+Menu menu = new Menu();
+Jatek();
+
+void Jatek()
+{
+    int v;
+    do
+    {
+        Console.ResetColor();
+        v = menu.MenuRajzol(new string[] { "Új játék", "Játék folytatása", "Játékleírás", "Kilépés" });
+        switch (v)
+        {
+            case 0:
+                Player player = new Player(0, 0, "P", new int[] { 20, 10 });
+                Map map = new Map(20, 10, 10, player);
+                 map.Run(); //await 
+                break;
+            case 1:
+                // fajliras + jatek
+                break;
+            case 2:
+                Kiir();
+                break;
+        }
+    } while (v != 3);
+    Console.ResetColor();
+}
+
+
+void Kiir(){
+    Console.WriteLine("Játékleírás:\nA játékot egy játékos vs. gép módban lehet játszani. \n" +
+        " A játék lényege ,hogy az egyik játékosnak minnél előbb el kell jutnia a másik játékos kezdő poziciójára. \n" +
+        "Az nyer aki hamarabb ér a másik kezdő helyzetére \n" +
+        "Három nehézségi kategória van: könnyű, küzepes, nehéz");
+}
